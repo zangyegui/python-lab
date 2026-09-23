355d1b6bc9e3e83b7194c7b1ac4ba3510de01388
zangyegui <2878468886@qq.com>
2026-09-23 21:49:31 +0800

    fix:fix the bug of ScoreRank

--- D4_containers/ScoreRank.py
+++ D4_containers/ScoreRank.py
@@ -1,4 +1,4 @@
-ls = [("Adam",59),("Bob",60),("Cur",63),("David",75),("Eliver",79),("Fuzz",80),("Grad",89),("Hery",94),("I",98)]
-ls.sort(reverse = True)
+ls = [("Adam",99),("Bob",60),("Cur",63),("David",75),("Eliver",79),("Fuzz",80),("Grad",89),("Hery",94),("I",64)]
+ls.sort(key = lambda x:x[1] ,reverse = True)
 for person,score in enumerate(ls):
-    print(person+1,score[0])
+    print(f"{person+1} {score[0]} {score[1]}")
