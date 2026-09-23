2d7add06ba311f0f72a18d58a7fd462eb7e61b0f
zangyegui <2878468886@qq.com>
2026-09-23 21:54:07 +0800

    fix:Compensate b and c for Rewriting program

--- D4_containers/DedutiveRewriting.py
+++ D4_containers/DedutiveRewriting.py
@@ -5,3 +5,10 @@
 #         result.append(x)
 result = [x for x in range(1,7) if x % 2 == 0]
 print(result)
+
+result1 = [x ** 2 for x in range(1,7) ]
+print(result1)
+
+ls = [("Adam",99),("Bob",60),("Cur",63),("David",75),("Eliver",79),("Fuzz",80),("Grad",89),("Hery",94),("I",64)]
+result2 = [x[0] for x in ls if x[1] >= 90]
+print(result2)
