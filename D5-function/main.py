497dfd3795d6fa7e0cf63f3a254a51ee33d737fe
zangyegui <2878468886@qq.com>
2026-09-26 14:07:57 +0800

    fix:standardlize utils function name

--- D5-function/main.py
+++ D5-function/main.py
@@ -1,7 +1,7 @@
-from utils import isprime,word_freq
+from utils import is_prime,word_freq
 
 for i in range(101):
-    if isprime(i):
+    if is_prime(i):
         print(i)
 
 s = "Mental rotation of visual stimuli is well studied. The key characteristic of mental rotation of visual stimuli is the linear relationship between the time required to perform a rotation task and the required angle of rotation, as shown first by Shepard and Metzler "
--- D5-function/utils.py
+++ D5-function/utils.py
@@ -1,4 +1,4 @@
-def isprime(n):
+def is_prime(n):
     if n < 2:
         return False
     for i in range(2,int(n ** 0.5)+1):
