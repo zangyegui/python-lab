832f4d6edb6bc8dc241c87ffbbbf0eb230d1b036
zangyegui <2878468886@qq.com>
2026-09-26 11:03:47 +0800

    fix:Convert bug

--- D5-function/ConvertTemperature.py
+++ D5-function/ConvertTemperature.py
@@ -3,15 +3,16 @@
 
 '欧式/华氏温度互转'
 
-def convert(value,str = "C"):
-    try:
-        if str == "C":
-            return value *9/5 +32
-        elif str == "F":
-            return (value -32) *5/9
-    except TypeError as e:
-        print("please input float temperature!")
-        return e
+def convert(value:float,unit:str = "C")->float:
+    """摄氏/华氏温度互转：unit='C' 输入摄氏，unit='F' 输入华氏"""
+    if not isinstance(value,(int,float)):
+        print("please input int/float value!")
+        return
+    if unit == "C":
+        return value *9/5 +32
+    elif unit == "F":
+        return (value -32) *5/9
+   
 
 if __name__ == '__main__':
     print(convert(37))
